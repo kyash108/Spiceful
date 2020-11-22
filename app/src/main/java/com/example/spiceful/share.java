e60d675b2dfb8dc7724b75024b20964e2c8a0ae7
Yash Kumar <w0749495@myscc.ca>
2020-11-22 04:56:21 -0500

    Made some changes in the share fragment

--- app/src/main/java/com/example/spiceful/share.java
+++ app/src/main/java/com/example/spiceful/share.java
@@ -61,7 +61,7 @@ public class share extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_share, container, false);
+        View view= inflater.inflate(R.layout.nav_header_main, container, false);
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
         String shareBody="Download this application from https://ykumar.scweb.ca/";
